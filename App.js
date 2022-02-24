b7568a09dbfbf895c7a57fcc33363e0a70f14c8d
arya2107 <69304191+arya2107@users.noreply.github.com>
2022-02-24 17:02:00 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import BottomTabNavigator from './components/bottomTabnavigater';
+export default class App extends Component {
+  render(){
+  return (
+<BottomTabNavigator></BottomTabNavigator>
+     );
+  }
+}
+
+
